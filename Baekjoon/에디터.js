c82caf432f941c5a1cfd92180c08590df41c9969
easyone <thinkanddoit@naver.com>
2021-12-20 19:20:45 +0900

    에디터

--- Baekjoon/에디터.js
+++ Baekjoon/에디터.js
@@ -7,13 +7,16 @@ const rightArray = [];
 const inputLineCount = +input.shift();
 
 for (let i = 0; i < inputLineCount; i++) {
-  if (input[i][0] === "L") rightArray.push(leftArray.pop());
-  else if (input[i][0] === "D") leftArray.push(rightArray.pop());
-  else if (input[i][0] === "B") leftArray.pop();
+  if (input[i][0] === "L") {
+    const fromLeft = leftArray.pop();
+    if (fromLeft) rightArray.push(fromLeft);
+  } else if (input[i][0] === "D") {
+    const fromRight = rightArray.pop();
+    if (fromRight) leftArray.push(fromRight);
+  } else if (input[i][0] === "B") leftArray.pop();
   else if (input[i][0] === "P") {
     const toAdd = input[i].split(" ")[1];
     leftArray.push(toAdd);
   }
 }
-
-console.log(leftArray, rightArray);
+console.log([...leftArray, ...rightArray.reverse()].join(""));
