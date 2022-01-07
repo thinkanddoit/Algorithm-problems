5d0ace0df4922d1eb68b6c78d522cee4443f87b0
easyone <thinkanddoit@naver.com>
2022-01-07 12:51:10 +0900

    오큰수

---
+++ Baekjoon/오큰수.js
@@ -0,0 +1,19 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(path).toString().trim().split("\n");
+
+const testNumber = Number(input.shift());
+const arr = input[0].split(" ").map((x) => Number(x));
+const stack = [0]; //초기항은 미리 넣어줌
+
+for (let i = 1; i < testNumber; i++) {
+  while (stack.length !== 0 && arr[stack[stack.length - 1]] < arr[i]) {
+    arr[stack.pop()] = arr[i];
+  }
+  stack.push(i);
+}
+//forEach 가 while 보다 시간을 더 잡아먹는 것 같다.
+while (stack.length !== 0) {
+  arr[stack.pop()] = -1;
+}
+
+console.log(arr.join(" "));
