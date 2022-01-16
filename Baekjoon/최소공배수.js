dcebc9a068200b3a244974e16cee1e6ef53edb9b
easyone <thinkanddoit@naver.com>
2022-01-16 23:25:55 +0900

    최소공배수

---
+++ Baekjoon/최소공배수.js
@@ -0,0 +1,26 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+
+const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const testNumber = input.shift();
+const LCMs = [];
+
+for (let i = 0; i < testNumber; i++) {
+  let [first, second] = input[i]
+    .split(" ")
+    .map((x) => Number(x))
+    .sort((a, b) => a - b);
+  let commonMutiple = [1];
+  for (let j = 2; j <= first; j++) {
+    if (first % j === 0 && second % j === 0) {
+      commonMutiple.push(j);
+      first /= j;
+      second /= j;
+      j = 1;
+    }
+  }
+
+  LCMs.push(commonMutiple.reduce((a, b) => a * b) * first * second);
+  console.log(first, second, commonMutiple);
+}
+
+console.log(LCMs.join("\n"));
