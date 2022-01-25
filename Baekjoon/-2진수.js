80b336cf89fd7afccebb578f9d6bf70ad528c7c4
easyone <thinkanddoit@naver.com>
2022-01-25 17:55:20 +0900

    진법 변환

--- Baekjoon/-2진수.js
+++ Baekjoon/-2진수.js
@@ -4,6 +4,10 @@ let number = Number(require("fs").readFileSync(path).toString().trim());
 let result = [];
 
 while (number != 0) {
+  console.log("number", number);
+  console.log("number/-2, ceil : ", Math.ceil(number / -2));
+  console.log("number%-2 : ", number % -2);
+
   result.push(Math.abs(number % -2));
   number = Math.ceil(number / -2);
 }
