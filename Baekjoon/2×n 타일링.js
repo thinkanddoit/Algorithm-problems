e35bcf522beecfce4715f617d1c8a0ad7cc2103e
easyone <thinkanddoit@naver.com>
2022-02-01 17:58:45 +0900

    2xn 타일링

--- Baekjoon/2×n 타일링.js
+++ Baekjoon/2×n 타일링.js
@@ -1,3 +1,14 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-const input = require("fs").readFileSync(path).toString();
+const number = +require("fs").readFileSync(path).toString().trim();
+
+let dp = new Array(number + 1).fill(0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= number; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[number]);
