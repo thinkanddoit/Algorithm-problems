dbae9bbfcc54e382f8e269d7a2fc6b984919942f
easyone <thinkanddoit@naver.com>
2022-02-02 10:24:58 +0900

    카드 구매하기 2

--- Baekjoon/카드 구매하기 2.js
+++ Baekjoon/카드 구매하기 2.js
@@ -3,17 +3,14 @@ const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
 const sum = +input.shift();
-let dp = new Array(sum + 1).fill(0);
+const numbers = input[0].split(" ").map((x) => +x);
+let dp = new Array(sum + 1).fill(10001);
 
-const numbers = input
-  .shift()
-  .split(" ")
-  .map((x) => +x);
+dp[0] = 0;
 
 for (let i = 1; i <= sum; i++) {
   for (let j = 1; j <= i; j++) {
-    dp[i] = Math.max(dp[i], dp[i - j] + numbers[j - 1]);
+    dp[i] = Math.min(dp[i], dp[i - j] + numbers[j - 1]);
   }
 }
-
 console.log(dp[sum]);
