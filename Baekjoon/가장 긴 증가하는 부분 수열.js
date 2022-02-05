4f277cf2700e7d2142edd92dca67629469c31c92
easyone <thinkanddoit@naver.com>
2022-02-05 20:55:31 +0900

    가장 긴 증가하는 부분 수열 n^2 방식

---
+++ Baekjoon/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,23 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+
+let [count, arr] = require("fs")
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split("\n");
+count = Number(count);
+arr = arr.split(" ").map((x) => Number(x));
+arr.unshift(0);
+
+const dp = new Array(count + 1).fill(0);
+
+dp[1] = 1;
+
+for (let i = 2; i <= count; i++) {
+  for (let j = 1; j < i; j++) {
+    if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+  }
+  if (dp[i] === 0) dp[i] = 1;
+}
+
+console.log(Math.max(...dp));
