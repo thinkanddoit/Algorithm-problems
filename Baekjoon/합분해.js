4a1b4712b7180a8cdd41b2e195a66da81a20ed1c
easyone <thinkanddoit@naver.com>
2022-02-10 00:36:52 +0900

    합분해

---
+++ Baekjoon/합분해.js
@@ -0,0 +1,29 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+
+const [N, K] = require("fs")
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split(" ")
+  .map((x) => +x);
+
+const dp = new Array(K + 1).fill(null).map(() => new Array(N + 1).fill(0));
+const Mod = 1000000000;
+
+for (let i = 0; i <= N; i++) {
+  dp[1][i] = 1;
+}
+
+console.log(dp[1]);
+
+for (let i = 2; i <= K; i++) {
+  for (let j = 0; j <= N; j++) {
+    let temp = 0;
+    for (let k = 0; k <= j; k++) {
+      temp += dp[i - 1][k];
+    }
+    dp[i][j] = temp % Mod;
+  }
+}
+
+console.log(dp[K][N]);
