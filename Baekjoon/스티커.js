9b70b217ae1311e45fac1f213c156deb55cdee59
easyone <thinkanddoit@naver.com>
2022-02-12 02:45:08 +0900

    스티커 수정

--- Baekjoon/스티커.js
+++ Baekjoon/스티커.js
@@ -9,10 +9,15 @@ for (let i = 0; i < testNumber * 3; i += 3) {
   const secondRow = input[i + 2].split(" ").map((x) => +x);
   const numbers = [firstRow, secondRow];
   const dp = Array.from(new Array(N + 1), () => new Array(2).fill(0));
-  dp[1][0] = numbers[0][0];
-  dp[1][1] = numbers[1][0];
-  dp[2][0] = dp[1][1] + numbers[0][1];
-  dp[2][1] = dp[1][0] + numbers[1][1];
+  if (N === 1) {
+    dp[1][0] = numbers[0][0];
+    dp[1][1] = numbers[1][0];
+  } else {
+    dp[1][0] = numbers[0][0];
+    dp[1][1] = numbers[1][0];
+    dp[2][0] = dp[1][1] + numbers[0][1];
+    dp[2][1] = dp[1][0] + numbers[1][1];
+  }
   for (let i = 3; i <= N; i++) {
     dp[i][0] = Math.max(dp[i - 1][1], dp[i - 2][1]) + numbers[0][i - 1];
     dp[i][1] = Math.max(dp[i - 1][0], dp[i - 2][0]) + numbers[1][i - 1];
