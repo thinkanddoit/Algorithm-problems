265e6ec7033a61c6b6d89a2751e80a7096b8e5d4
easyone <thinkanddoit@naver.com>
2022-04-30 19:26:26 +0900

    오픈채팅방 풀이 추가

--- Programmers/오픈채팅방.js
+++ Programmers/오픈채팅방.js
@@ -27,3 +27,30 @@ function solution(record) {
 }
 
 solution(input);
+
+// 더 좋은 풀이 연습
+
+function solution2(record) {
+  const userInfo = {};
+  const action = [];
+  const stateMapping = {
+    Enter: '님이 들어왔습니다.',
+    Leave: '님이 나갔습니다.',
+  }; // 이 부분이 내가 생각해뒀던 부분..
+
+  record.forEach((v) => {
+    const [state, id, nick] = v.split(' ');
+
+    if (state !== 'Change') {
+      action.push([state, id]);
+    }
+
+    if (nick) {
+      userInfo[id] = nick;
+    }
+  });
+
+  return action.map(([state, uid]) => {
+    return `${userInfo[uid]}${stateMapping[state]}`;
+  });
+}
